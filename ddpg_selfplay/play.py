4d945f79cc2aa6cf8905131d430d95ea7b58349e
Felipe <felipemartins96@gmail.com>
2021-02-13 14:48:15 -0300

    add self_play agent

---
+++ ddpg_selfplay/play.py
@@ -0,0 +1,76 @@
+import os
+import argparse
+import collections
+
+import gym
+import ptan
+import time
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.multiprocessing as mp
+from tensorboardX import SummaryWriter
+
+import rc_gym
+
+ENV = 'VSS1v1SelfPlay-v0'
+PROCESSES_COUNT = 3
+LEARNING_RATE = 0.0001
+REPLAY_SIZE = 2000000
+REPLAY_INITIAL = 100000
+BATCH_SIZE = 256
+GAMMA = 0.95
+REWARD_STEPS = 2
+SAVE_FREQUENCY = 200000
+# Q_SIZE = 1
+
+
+class DDPGActor(nn.Module):
+    def __init__(self, obs_size, act_size):
+        super(DDPGActor, self).__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(obs_size, 400),
+            nn.ReLU(),
+            nn.Linear(400, 300),
+            nn.ReLU(),
+            nn.Linear(300, act_size),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", required=True, help="Model file to load")
+    parser.add_argument("-e", "--env", default=ENV, help="Environment name to use, default=" + ENV)
+    parser.add_argument("-r", "--record", help="If specified, sets the recording dir, default=Disabled")
+    args = parser.parse_args()
+
+    env = gym.make(args.env)
+
+    net = DDPGActor(40, 2)
+    net.load_state_dict(torch.load(args.model))
+
+    obs = env.reset()
+    total_reward = 0.0
+    total_steps = 0
+    while True:
+        obs_v = torch.FloatTensor(obs)
+        mu_v = net(obs_v)
+        # print(mu_v)
+        action = mu_v.squeeze(dim=0).data.numpy()
+        # print(action)
+        action = np.clip(action, -1, 1)
+        obs, reward, done, _ = env.step(action)
+        env.env.render()
+        total_reward += reward[0]
+        total_steps += 1
+        if done[0]:
+            break
+    print("In %d steps we got %.3f reward" % (total_steps, total_reward))
