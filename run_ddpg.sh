6f7fe05bfa2fa0c3b5e682da1fa85fb85b2dbbe9
goncamateus <mgm4@cin.ufpe.br>
2022-11-17 19:18:26 -0300

    ran withaction-skip3-stack10-step0.01

--- run_ddpg.sh
+++ run_ddpg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in 0; do
-  python train_ddpg.py --cuda -n "DDPG-5" -e VSS-v0
+  python train_ddpg.py --cuda -n "WithAction-Stack10-step0.01" -e VSS-v0
 done
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import rsoccer_gym
 import numpy as np
 
 from agents.ddpg import DDPGActor
-from agents.utils.wrappers import FrameStack, ObsWithActionWrapper
+from agents.utils.wrappers import FrameStack, ObsWithActionWrapper, SkipFrameWrapper
 
 import torch
 
@@ -15,11 +15,11 @@ obs = env.reset()
 
 # print(env.action_space.shape)
 model = DDPGActor(obs.shape[0], env.action_space.shape[0])
-model.load_state_dict(torch.load('checkpoint_000750000.pth')['pi_state_dict'])
+model.load_state_dict(torch.load('/home/gonca/rl/rl-agents-pytorch/saves/VSS-v0/ddpg_async/WithAction-Skip3-Stack10-step0.01/checkpoints/checkpoint_000550000.pth')['pi_state_dict'])
 
 obs = torch.FloatTensor(obs)
 print(model)
 
 traced_script_module = torch.jit.trace(model, obs)
 
-torch.jit.save(traced_script_module, 'atk-4.pt')
+torch.jit.save(traced_script_module, 'WithAction-Skip3-Stack10-step0.01.pt')
